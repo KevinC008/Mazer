84c1a8e041b13a6b31ee847a82554fb348328361
KevinC008 <72290176+KevinC008@users.noreply.github.com>
2020-11-18 18:16:54 +0800

    Update default_value.cpp

--- default_value.cpp
+++ default_value.cpp
@@ -1,6 +1,6 @@
 #include "datas.h"
-status Player;
-status Monster;
+status Kid;
+status Giant;
 map<string, status> items;
 
 status MakeStatus(int x ,int y ,int h ,int d , int v){
@@ -14,10 +14,10 @@ status MakeStatus(int x ,int y ,int h ,int d , int v){
 }
 
 void define_items(){
-  items["Accelerator-Front"] = MakeStatus(0,-2,0,0,0);
-  items["Accelerator-Back"] = MakeStatus(0,2,0,0,0);
-  items["Accelerator-Left"] = MakeStatus(-2,0,0,0,0);
-  items["Accelerator-Right"] = MakeStatus(2,0,0,0,0);
+  items["Front"] = MakeStatus(0,-2,0,0,0);
+  items["Back"] = MakeStatus(0,2,0,0,0);
+  items["Left"] = MakeStatus(-2,0,0,0,0);
+  items["Right"] = MakeStatus(2,0,0,0,0);
   items["Potion(HP +5)"] = MakeStatus(0,0,5,0,0);
   items["Bandage(HP +5)"] = MakeStatus(0,0,5,0,0);
   items["Potion(HP +10)"] = MakeStatus(0,0,10,0,0);
@@ -28,10 +28,10 @@ void define_items(){
   items[" NULL"] = MakeStatus(0,0,0,0,0);
 }
 
-void define_player_monster(int S){
+void define_kid_giant(int S){
   srand(time(NULL));
-  Player = MakeStatus(S-1,S-1,S*4,S,1);
-  Monster = MakeStatus(rand()%(S-2),rand()%(S-2),S*16,S*2,0);
+  Kid = MakeStatus(S-1,S-1,S*4,S,1);
+  Giant = MakeStatus(rand()%(S-2),rand()%(S-2),S*16,S*2,0);
 }
 
 string random_item(int k){
